e3a9992c96803519acab2fed847590b32e405427
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-02-12 20:46:49 +0530

    Add files via upload

---
+++ FindAngleMBC.py
@@ -0,0 +1,22 @@
+"""
+Title     : Find Angle MBC
+Subdomain : Math
+Domain    : Python
+"""
+
+
+import math
+
+ab = float(input())
+bc = float(input())
+ac = math.sqrt(ab**2 + bc**2)
+bm = ac / 2.0
+mc = bm
+# let,
+b = mc
+c = bm
+a = bc
+# where b=c
+angel_b_radian = math.acos(a / (2 * b))
+angel_b_degree = int(round((180 * angel_b_radian) / math.pi))
+print(f"{angel_b_degree}°")
